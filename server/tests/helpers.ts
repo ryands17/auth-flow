b5eb837719507a65af74049253da8452be29da75
Ryan <dsouza.ryan.11@gmail.com>
2020-10-21 18:05:32 +0530

    login and authenticated endpoint tests

--- server/tests/helpers.ts
+++ server/tests/helpers.ts
@@ -1,12 +1,25 @@
+import { FastifyInstance } from 'fastify'
 import { app, prisma } from '../src/app'
+import cookieParser from 'fastify-cookie'
+
+const start = async (app: FastifyInstance) => {
+  try {
+    app.register(cookieParser)
+  } catch (err) {
+    console.log('e', err)
+    process.exit(1)
+  }
+}
 
 beforeAll(async done => {
+  await start(app)
   await app.ready()
   done()
 })
 
 afterAll(async done => {
   await prisma.$disconnect()
+  await app.close()
   done()
 })
 
--- server/tests/user-auth.test.ts
+++ server/tests/user-auth.test.ts
@@ -1,6 +1,8 @@
 import supertest from 'supertest'
 import { app } from './helpers'
 
+let accessToken = ''
+
 test('signup', async () => {
   const res = await supertest(app.server)
     .post('/signup')
@@ -22,4 +24,31 @@ test('signup', async () => {
       },
     },
   })
+})
+
+test('login', async () => {
+  const res = await supertest(app.server)
+    .post('/login')
+    .send({
+      email: 'user@g.com',
+      password: 'password',
+    })
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200)
+
+  expect(res.body).toHaveProperty('data.user')
+  expect(res.body).toHaveProperty('data.accessToken')
+  accessToken = res.body.data.accessToken
+})
+
+test('list users', async () => {
+  const res = await supertest(app.server)
+    .get('/users')
+    .set({ Authorization: `Bearer ${accessToken}`, Accept: 'application/json' })
+    .expect('Content-Type', /json/)
+    .expect(200)
+
+  expect(res.body).toHaveProperty('data.users')
+  expect(res.body.data.users).toHaveLength(1)
 })
